dc74acf874225510ced7fad43fe0ea350c8c167b
pacimarco <m.paci1987@gmail.com>
2022-09-26 10:33:13 +0200

    prova stampa trait

--- index.php
+++ index.php
@@ -13,11 +13,17 @@ echo $cliente1->getFullName();
 $agente1 = new Agent("Ugo","bianchi","ugo.agentii@info.it","338667888","via del campo 1b","1444433");
 echo $agente1->getFullName();
 
+
+//ora devo cambiare inserimento dopo la creazione della trait//
 $immobili1 = new propertyForRent("via grande 9, RM","monolocal","furnished","40m","500euro","disponibile");
 
 echo $immobili1->houseInfo();//stampa info
 
 $immobili1->availability("disponibile");//stampa disponibilità
+
+
+$immobile2= new propertyForRent("via como","32","Roma","Rm","01790");
+echo $immobili2->setIndirizzo();//stampa info
 
 //prove uso Trait ---da rivedere
 
